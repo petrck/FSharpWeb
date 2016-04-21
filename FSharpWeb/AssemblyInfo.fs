276a65794aafdd10c8851ea6536f5f4c8d201bf7
petrck <petr.ck@seznam.cz>
2016-04-21 23:38:36 +0200

    first commit

---
+++ FSharpWeb/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpWeb
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FSharpWeb")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("petrtomasek")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
